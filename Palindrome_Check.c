9a37bbf8e9522267ca450ed16137042c5f28a17a
F0x <Operator-F0x@proton.me>
2024-05-26 12:57:56 +0200

    Create Palindrome_Check.c

---
+++ Palindrome_Check.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Function to check if a string is a palindrome
+bool isPalindrome(char str[]) {
+    int l = 0;
+    int h = strlen(str) - 1;
+
+    while (h > l) {
+        if (str[l++] != str[h--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    char str[] = "madam";
+    if (isPalindrome(str))
+        printf("%s is a palindrome\n", str);
+    else
+        printf("%s is not a palindrome\n", str);
+    return 0;
+}
