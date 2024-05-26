16970498f3f6e8d1a279de0c8733b69e4c099128
F0x <Operator-F0x@proton.me>
2024-05-26 12:58:52 +0200

    Create Linked_List_Insertion.c

---
+++ Linked_List_Insertion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for singly linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert a new node at the beginning
+void insertAtBeginning(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+// Function to print the linked list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    insertAtBeginning(&head, 1);
+    insertAtBeginning(&head, 2);
+    insertAtBeginning(&head, 3);
+    insertAtBeginning(&head, 4);
+
+    printf("Linked list: ");
+    printList(head);
+
+    return 0;
+}
